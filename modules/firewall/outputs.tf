55709ea1db1e042ca8e1be3e76ac2a03575c1977
Javier Alonso <jackarrillo.1@gmail.com>
2023-01-12 16:40:52 -0500

    update firewall_rule

--- modules/firewall/outputs.tf
+++ modules/firewall/outputs.tf
@@ -1,13 +1,3 @@
 output "firewall_rule" {
   value = "${google_compute_firewall.fr_allow_internal.name}"
 }
-
-output "firewall_rule" {
-  value = "${google_compute_firewall.fr_allow_ssh.name}"
-}
-
-output "firewall_rule" {
-  value = "${google_compute_firewall.fr_allow_icmp.name}"
-}
-
-
