354bddef5cf0f050d3efb4f58ce1dcf98e59d07d
Javier Alonso <jackarrillo.1@gmail.com>
2023-01-16 12:02:40 -0500

    terraform fr-2

--- modules/firewall/main.tf
+++ modules/firewall/main.tf
@@ -2,6 +2,22 @@ locals {
   network = "${element(split("-", var.subnet), 0)}"
 }
 
+resource "google_compute_firewall" "fr_allow_ssh" {
+  name        = "${local.network}-allow-ssh"
+  network     = "${local.network}"
+  project     = "${var.project}"
+  priority    = 65534
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  target_tags   = ["ssh-connect"]
+  source_ranges = ["0.0.0.0/0"]
+  
+}
+
 resource "google_compute_firewall" "fr_allow-internal" {
   
   direction   = "INGRESS"
@@ -22,21 +38,6 @@ resource "google_compute_firewall" "fr_allow-internal" {
   source_ranges = ["10.10.10.0/24", "10.110.0.0/24"]
 }
 
-resource "google_compute_firewall" "fr_allow_ssh" {
-  name        = "${local.network}-allow-ssh"
-  network     = "${local.network}"
-  project     = "${var.project}"
-  priority    = 65534
-
-  allow {
-    protocol = "tcp"
-    ports    = ["22"]
-  }
-
-  target_tags   = ["ssh-connect"]
-  source_ranges = ["0.0.0.0/0"]
-  
-}
 
 resource "google_compute_firewall" "fr_allow_icmp" {
   allow {
